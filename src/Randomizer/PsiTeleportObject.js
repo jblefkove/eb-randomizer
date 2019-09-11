681876f94d68e5863e05092ce6d87d081ed0dea6
Peter Branam-Lefkove <peter.branam-lefkove@libertyits.com>
2019-09-11 15:49:18 -0400

    More Keysanity items and better softlock protection

--- src/Randomizer/PsiTeleportObject.js
+++ src/Randomizer/PsiTeleportObject.js
@@ -48,20 +48,144 @@ class PsiTeleportObject extends TableObject {
         }
     }
 
+    static containsAll (array, other) {
+        for (var i = 0; i < other.length; i++) {
+            if (array.indexOf(other[i]) === -1) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     static checkLegalKeysanity() {
         if(!this._results) return false;
-        const invalidPairs = [
-            [0xb4, 0x01], // Franklin badge at Wad of bills
-            [0x01, 0xb8], // Pencil eraser at Franklin badge
-            [0xb4, 0xb8], // Pencil eraser at Wad of bills
-            [0xfd, 0xb7], // Signed banana at Carrot key
-            [0xfd, 0xb6], // Diamond at Carrot key
-            [0xa6, 0xb6], // Diamond at King banana
-            [0xb7, 0xb6], // Diamond at Signed banana
-            [0xd3, 0xa4], // Shyness book at Tendakraut
-        ];
-        return this._results.every(resultPair => !invalidPairs.some(invalidPair => 
-            invalidPair[0] === resultPair[0] && invalidPair[1] === resultPair[1] ));
+
+        const FRANKLIN_BADGE = 0x01;
+        const JAR_OF_FLY_HONEY = 0x69;
+        const BACKSTAGE_PASS = 0x7d;
+        const YOGURT_DISPENSER = 0x8b;
+        const FOR_SALE_SIGN = 0xa3;
+        const SHYNESS_BOOK = 0xa4;
+        const KING_BANANA = 0xa6;
+        const KEY_TO_THE_SHACK = 0xaa;
+        const KEY_TO_THE_CABIN = 0xab;
+        const HAWK_EYE = 0xaf;
+        const BICYCLE = 0xb0;
+        const WAD_OF_BILLS = 0xb4;
+        const RECEIVER_PHONE = 0xb5;
+        const DIAMOND = 0xb6;
+        const SIGNED_BANANA = 0xb7;
+        const PENCIL_ERASER = 0xb8;
+        const HIEROGLYPH_COPY = 0xb9;
+        const CONTACT_LENS = 0xbb;
+        const KEY_TO_THE_TOWER = 0xc0;
+        const METEORITE_PIECE = 0xc1;
+        const SOUND_STONE = 0xc4;
+        const TOWN_MAP = 0xca;
+        const SUPORMA = 0xcc;
+        const INSIGNIFICANT_ITEM = 0xce;
+        const ERASER_ERASER = 0xd2;
+        const TENDAKRAUT = 0xd3;
+        const CARROT_KEY = 0xfd;
+
+        // JAR_OF_FLY_HONEY and METEORITE_PIECE are excluded
+        const LOCATIONS =
+          [
+            FRANKLIN_BADGE,
+            BACKSTAGE_PASS,
+            YOGURT_DISPENSER,
+            FOR_SALE_SIGN,
+            SHYNESS_BOOK,
+            KING_BANANA,
+            KEY_TO_THE_SHACK,
+            KEY_TO_THE_CABIN,
+            HAWK_EYE,
+            BICYCLE,
+            WAD_OF_BILLS,
+            RECEIVER_PHONE,
+            DIAMOND,
+            SIGNED_BANANA,
+            PENCIL_ERASER,
+            HIEROGLYPH_COPY,
+            CONTACT_LENS,
+            KEY_TO_THE_TOWER,
+            SOUND_STONE,
+            TOWN_MAP,
+            SUPORMA,
+            INSIGNIFICANT_ITEM,
+            ERASER_ERASER,
+            TENDAKRAUT,
+            CARROT_KEY,
+          ];
+
+        const REQUIRED_ITEMS =
+          [
+            KEY_TO_THE_SHACK,
+            PENCIL_ERASER,
+            JAR_OF_FLY_HONEY,
+            SIGNED_BANANA,
+            CARROT_KEY,
+            SHYNESS_BOOK,
+          ];
+
+        const LOCATION_REQUIREMENTS =
+          {
+            [FRANKLIN_BADGE] : [PENCIL_ERASER],
+            [KEY_TO_THE_CABIN] : [PENCIL_ERASER, FRANKLIN_BADGE],
+            [WAD_OF_BILLS] : [PENCIL_ERASER, FRANKLIN_BADGE, KEY_TO_THE_CABIN],
+            [BACKSTAGE_PASS] : [PENCIL_ERASER, FRANKLIN_BADGE, KEY_TO_THE_CABIN],
+            [SIGNED_BANANA] : [DIAMOND],
+            [KING_BANANA] : [DIAMOND],
+            [YOGURT_DISPENSER] : [DIAMOND, KING_BANANA, PENCIL_ERASER],
+            [CARROT_KEY] : [SIGNED_BANANA],
+            [SHYNESS_BOOK] : [ERASER_ERASER],
+            [TENDAKRAUT] : [SHYNESS_BOOK],
+          };
+
+        var openPositions = [];
+        for (var i = 0; i < LOCATIONS.length; i++) {
+          const pos = LOCATIONS[i];
+          if (!LOCATION_REQUIREMENTS[pos]) {
+            openPositions.push(pos);
+          }
+        }
+
+        var openItems = [];
+        for (var i = 0; i < this._results.length; i++) {
+          const item = this._results[i][1];
+          const dest = this._results[i][0];
+          if (openPositions.includes(dest)) {
+            openItems.push(item);
+          }
+        }
+
+        while (true) {
+          if (this.containsAll(openItems, REQUIRED_ITEMS)) {
+            return true;
+          }
+
+          var changed = false;
+
+          // try to open other items
+          for (var i = 0; i < this._results.length; i++) {
+            const item = this._results[i][1];
+            const dest = this._results[i][0];
+            if (openItems.indexOf(item) !== -1) {
+              continue;
+            }
+
+            if (this.containsAll(openItems, LOCATION_REQUIREMENTS[dest])) {
+              openItems.push(item);
+              openPositions.push(dest);
+              changed = true;
+            }
+          }
+
+          if (!changed) {
+            console.log("softlocked!");
+            return false;
+          }
+      }
     }
 
     static intershuffle() {
@@ -71,18 +195,28 @@ class PsiTeleportObject extends TableObject {
         const keyItemsIndex = [
             0x01,   // Franklin badge
             //0x69, // Jar of Fly Honey - Chest handled differently, at 0x7dacb.
+            0x7d,   // Backstage pass
+            0x8b,   // Yogurt dispenser
+            0xa3,   // For sale sign
             0xa4,   // Shyness book
             0xa6,   // King banana
             0xaa,   // Key to the shack
+            0xab,   // Key to the cabin
             0xaf,   // Hawk eye
             0xb0,   // Bicycle
             0xb4,   // Wad of bills
+            0xb5,   // Receiver phone
             0xb6,   // Diamond
             0xb7,   // Signed banana
             0xb8,   // Pencil eraser
+            0xb9,   // Hieroglyph copy
+            0xbb,   // Contact lens
             0xc0,   // Key to the tower
+            0xc4,   // Sound Stone
             0xca,   // Town map
             0xcc,   // Suporma
+            0xce,   // Insignificant item
+            0xd2,   // Eraser eraser
             0xd3,   // Tendakraut
             0xfd,   // Carrot key
         ];
@@ -130,7 +264,7 @@ class PsiTeleportObject extends TableObject {
         bubbleMonkeyRope.lines.splice(1, bubbleMonkeyRope.lines.length - 3);
         bubbleMonkeyRope.writeScript();
 
-        // Patch intro script to set all teleports available immediately, and 
+        // Patch intro script to set all teleports available immediately, and
         // put all necessary items in Tracy's inventory if Open mode
         const intro = Script.getByPointer(0x5e70b);
         let patchLines = intro.lines.slice(0,2);
